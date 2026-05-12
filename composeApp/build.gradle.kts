262d9d74e6c85ebf8ebeb4c0a124dee4fa05fd11
Vivek Sharma <vivek.sharma@mpokket.com>
2026-05-12 19:03:56 +0530

    Initial commit

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,97 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+//    alias(libs.plugins.google.services)
+//    id("io.kotzilla.kotzilla-plugin")
+}
+
+kotlin {
+    androidTarget {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(compose.preview)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.splash.screen)
+            implementation(libs.koin.android)
+        }
+        commonMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
+            implementation(compose.components.uiToolingPreview)
+            implementation(libs.androidx.lifecycle.viewmodel)
+            implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(libs.auth.kmp)
+            implementation(libs.firebase.app)
+
+            implementation(libs.koin.compose)
+
+            api(libs.kmp.notifier)
+
+//            implementation("io.kotzilla:kotzilla-sdk:1.2.0-Beta3")
+//            implementation("io.kotzilla:kotzilla-sdk-ktor3:1.2.0-Beta1")
+
+//            implementation(project(path = ":navigation"))
+//            implementation(project(path = ":shared"))
+//            implementation(project(path = ":di"))
+//            implementation(project(path = ":data"))
+        }
+        commonTest.dependencies {
+        }
+    }
+}
+
+android {
+    namespace = "com.hypheno.nutrisport"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.hypheno.nutrisport"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    debugImplementation(compose.uiTooling)
+}
---
+++ composeApp/src/iosMain/kotlin/com/hypheno/nutrisport/MainViewController.kt
@@ -0,0 +1,5 @@
+package com.hypheno.nutrisport
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
